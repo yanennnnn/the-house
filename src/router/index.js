ade71cdd6ed29953c4e23cff0dadb70d3d6f48da
yanen Chen <sh5ojkmt@gmail.com>
2020-07-29 11:33:23 +0800

    修正router

--- src/router/index.js
+++ src/router/index.js
@@ -4,31 +4,30 @@ import Home from '../views/Index.vue';
 
 Vue.use(VueRouter);
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home,
-  },
-  {
-    path: '/singleRoom/:id',
-    name: 'singleroom',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import('../views/SingleRoom.vue'),
-  },
-  {
-    path: '/reservation/:id',
-    name: 'reservation',
-    component: () => import('../views/Reservation.vue'),
-  },
-];
-
-const router = new VueRouter({
+export default new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes,
+  scrollBehavior() {
+    return { x: 0, y: 0 };
+  },
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home,
+    },
+    {
+      path: '/singleRoom/:id',
+      name: 'singleroom',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/SingleRoom.vue'),
+    },
+    {
+      path: '/reservation/:id',
+      name: 'reservation',
+      component: () => import('../views/Reservation.vue'),
+    },
+  ],
 });
-
-export default router;
