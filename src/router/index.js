bde526153eab335b57eef261069ec3821d2bd666
yanen Chen <sh5ojkmt@gmail.com>
2020-07-29 11:46:12 +0800

    修正網址

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Index.vue';
+import Home from '@/views/Index.vue';
 
 Vue.use(VueRouter);
 
@@ -22,12 +22,12 @@ export default new VueRouter({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/SingleRoom.vue'),
+      component: () => import('@/views/SingleRoom.vue'),
     },
     {
       path: '/reservation/:id',
       name: 'reservation',
-      component: () => import('../views/Reservation.vue'),
+      component: () => import('@/views/Reservation.vue'),
     },
   ],
 });
